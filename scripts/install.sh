77ce3ff8955f4f7847d2139edfb233d9de3307d6
HanmaDevin <devinbc14@gmail.com>
2025-08-18 15:03:31 +0200

    init

---
+++ scripts/install.sh
@@ -0,0 +1,232 @@
+#! /bin/bash
+#    ____           __        ____   _____           _       __ 
+#    /  _/___  _____/ /_____ _/ / /  / ___/__________(_)___  / /_
+#    / // __ \/ ___/ __/ __ `/ / /   \__ \/ ___/ ___/ / __ \/ __/
+#  _/ // / / (__  ) /_/ /_/ / / /   ___/ / /__/ /  / / /_/ / /_  
+# /___/_/ /_/____/\__/\__,_/_/_/   /____/\___/_/  /_/ .___/\__/  
+#                                                  /_/           
+clear
+
+from="$HOME/hyprdev/"
+cfgPath="$from/.config/"
+
+packages=(
+  "git"
+  "gum"
+  "zip"
+  "unzip"
+  "curl"
+  "wget"
+  "btop"
+  "mpv"
+  "mpv-mpris"
+  "neovim"
+  "nodejs-lts-jod"
+  "zsh"
+  "fastfetch"
+  "glow"
+  "discord"
+  "lazygit"
+  "ufw"
+  "yazi"
+  "eza"
+  "steam"
+  "zoxide"
+  "fzf"
+  "bat"
+  "jdk-openjdk"
+  "docker"
+  "ripgrep"
+  "cargo"
+  "fd"
+  "starship"
+  "wine"
+  "ttf-font-awesome"
+  "ttf-nerd-fonts-symbols"
+  "ttf-jetbrains-mono-nerd"
+  "noto-fonts-emoji"
+  "7izp"
+  "texlive"
+  "net-tools"
+  "inetutils"
+)
+
+aur_paqkages=(
+  "google-chrome"
+  "lazydocker"
+  "ani-cli"
+  "luajit-tiktoken-bin"
+  )
+
+installPackages() {
+  for pgk in "${packages[@]}"; do
+    if ! pacman -Qi "$pgk" &> /dev/null; then
+      sudo pacman --noconfirm -S "$pgk"
+    else
+      echo ":: $pgk is already installed."
+    fi
+  done
+}
+
+installAurPackages() {
+  for aur_pkg in "${aur_paqkages[@]}"; do
+    if ! pacman -Qi "$aur_pkg" &> /dev/null; then
+      echo ":: Installing $aur_pkg..."
+      yay --noconfirm -S "$aur_pkg"
+    else
+      echo "$aur_pkg is already installed."
+    fi
+  done
+  echo ":: All AUR packages have been installed successfully."
+}
+
+installYay() {
+    git clone https://aur.archlinux.org/yay.git $HOME/yay
+    cd $HOME/yay
+    makepkg -si
+    echo ":: yay has been installed successfully."
+}
+
+configure_git() {
+  echo ":: Want to configure git?"
+  answer=$(gum choose "Yes" "No")
+  if [[ "$answer" == "Yes" ]]; then
+    username=$(gum input --prompt ":: > What is your user name?")
+    git config --global user.name "$username"
+    useremail=$(gum input --prompt ":: > What is your email?")
+    git config --global user.email "$useremail"
+    git config --global pull.rebase true
+  fi
+
+  echo ":: Want to create a ssh-key?"
+  ssh=$(gum choose "Yes" "No")
+  if [[ "$ssh" == "Yes" ]]; then
+    ssh-keygen -t ed25519 -C "$useremail"
+  fi
+
+  echo ":: Want to create a physical key (Yubikey)?"
+  key=$(gum choose "Yes" "No")
+  if [[ "$key" == "Yes" ]]; then
+    read -r -p ":: Insert a device like a Yubikey and press enter..."
+    ssh-keygen -t ecdsa-sk -b 521
+  fi
+}
+
+detect_nvidia() {
+  gpu=$(lspci | grep -i '.* vga .* nvidia .*')
+
+  shopt -s nocasematch
+
+  if [[ $gpu == *' nvidia '* ]]; then
+    echo ":: Nvidia GPU is present"
+    gum spin --spinner dot --title "Installaling nvidia drivers now..." -- sleep 2
+    sudo pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+  else
+    echo "It seems you are not using a Nvidia GPU"
+    echo "If you have a Nvidia GPU then download the drivers yourself please :)"
+  fi
+}
+
+config_ufw() {
+  gum spin --spinner dot --title "Firewall will be configured..." -- sleep 2
+  sudo ufw enable
+  sudo ufw default deny incoming
+  sudo ufw default allow outgoing
+  sudo ufw status verbose
+}
+
+copy_config() {
+  gum spin --spinner dot --title "Creating bakups..." -- sleep 2
+
+  if [[ -f "$HOME/.zshrc" ]]; then 
+    mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+  fi
+
+  if [[ -d "$HOME/.config" ]]; then 
+    mv "$HOME/.config" "$HOME/.config.bak"
+  fi
+
+  sudo cp "$from/scripts/pullall.sh" "/usr/bin/"
+
+  if [[ ! -d "$HOME/Pictures/Screenshots/" ]]; then 
+    mkdir -p "$HOME/Pictures/Screenshots/"
+  fi
+
+  cp "$from/.zshrc" "$HOME/"
+  cp -r "$cfgPath" "$HOME/"
+
+  sudo cp -r "$from/icons/" "/usr/share/"
+ 
+  echo ":: Want to install Vencord?"
+  vencord=$(gum choose "Yes" "No")
+
+  if [[ "$vencord" == "Yes" ]]; then 
+    bash "$from/Vencord/VencordInstaller.sh"
+    cp -r "$from/Vencord/themes/" "$HOME/.config/Vencord/"
+  fi
+}
+
+add_tmux_tpm() {
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  mkdir -p ~/.config/tmux/plugins/catppuccin 
+  git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+}
+
+MAGENTA='\033[0;35m'
+NONE='\033[0m'
+
+# Header
+echo -e "${MAGENTA}"
+cat <<"EOF"
+   ____         __       ____
+  /  _/__  ___ / /____ _/ / /__ ____
+ _/ // _ \(_-</ __/ _ `/ / / -_) __/
+/___/_//_/___/\__/\_,_/_/_/\__/_/
+
+EOF
+
+echo "Post Manjaro Installation Script"
+echo -e "${NONE}"
+while true; do
+    read -p ":: DO YOU WANT TO START THE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]*)
+            echo ":: Installation started."
+            echo
+            break
+            ;;
+        [Nn]*)
+            echo ":: Installation canceled"
+            exit
+            break
+            ;;
+        *)
+            echo ":: Please answer yes or no."
+            ;;
+    esac
+done
+
+sudo pacman -Syu
+
+# Install required packages
+echo ":: Installing required packages..."
+installPackages
+installYay
+installAurPackages
+
+gum spin --spinner dot --title "Starting setup now..." -- sleep 2
+copy_config
+detect_nvidia
+configure_git
+add_tmux_tpm
+
+echo -e "${MAGENTA}"
+cat <<"EOF"
+    ____  __                        ____       __                __ 
+   / __ \/ /__  ____ _________     / __ \___  / /_  ____  ____  / /_
+  / /_/ / / _ \/ __ `/ ___/ _ \   / /_/ / _ \/ __ \/ __ \/ __ \/ __/
+ / ____/ /  __/ /_/ (__  )  __/  / _, _/  __/ /_/ / /_/ / /_/ / /_  
+/_/   /_/\___/\__,_/____/\___/  /_/ |_|\___/_.___/\____/\____/\__/  
+EOF
+echo "and thank you for choosing my config :)"
+echo -e "${NONE}"
---
+++ scripts/pullall.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script aims to traverse all subdirectories and execute 'git pull' in each one.
+
+dirs=$(fd --exclude "go" --exclude "yay" --exclude ".vscode" --exclude ".local" --exclude ".config" --exclude ".tmux" -H "^(\.git)$" ~ --exec echo {//})
+
+for dir in $dirs; do
+    if [ -d "$dir" ]; then
+        echo "Pulling updates in: $dir"
+        cd "$dir"
+        if [[ $(git status | grep "Changes" | wc -l) -gt 0 ]]; then
+            git stash
+        fi
+
+        git pull
+
+        if [[ $(git stash list | wc -l) -gt 0 ]]; then
+            git stash pop
+        fi
+        cd - > /dev/null
+    else
+        echo "Directory $dir does not exist."
+    fi
+done
